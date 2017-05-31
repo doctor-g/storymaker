a422bdb49b152d8a60edaaf6fd91088e30e43635
Travis CI <travis@travis-ci.org>
2017-05-31 19:04:17 +0000

    Deploy to GitHub Pages: dbb6c22115dd26ac05bb640541cbe54db8e9a8b4

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","930b6c9a559ed252da166726ca573dd1"]];
+var precacheConfig = [["index.html","8442ad12803b23f07320f292237dea8d"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
